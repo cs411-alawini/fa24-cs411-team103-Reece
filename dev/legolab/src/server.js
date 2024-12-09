32f205889db6a15104e6efaecd8948908200944d
flash1405 <guptaaryan1405@gmail.com>
2024-12-08 23:56:46 -0600

    Data dump

--- dev/legolab/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-require("dotenv").config();
-
-const PORT = 5000;
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
